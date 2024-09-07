5cc326719f01773da3df390943a78cc96a2fd140
Atsushi Eno <atsushieno@gmail.com>
2024-09-07 13:19:55 +0900

    app: use LibreMidiAccess on macos and linux.

--- app/src/desktopMain/kotlin/main.kt
+++ app/src/desktopMain/kotlin/main.kt
@@ -4,17 +4,28 @@ import androidx.compose.ui.window.Window
 import androidx.compose.ui.window.application
 import androidx.compose.ui.window.rememberWindowState
 import dev.atsushieno.ktmidi.JvmMidiAccess
+import dev.atsushieno.ktmidi.LibreMidiAccess
+import dev.atsushieno.ktmidi.MergedMidiAccess
+import dev.atsushieno.ktmidi.MidiTransportProtocol
 import org.androidaudioplugin.composeaudiocontrols.demoapp.MainContent
 import org.androidaudioplugin.composeaudiocontrols.demoapp.midiAccess
 
-fun main(args: Array<String>) = application {
-    midiAccess = JvmMidiAccess()
-    Window(onCloseRequest = ::exitApplication,
-        state = rememberWindowState(),
-        title = "ComposeAudioControls Demo") {
-        MainContent()
+fun main(args: Array<String>) {
+    midiAccess = when {
+        System.getProperty("os.name").startsWith("Windows", true) -> JvmMidiAccess()
+        else -> LibreMidiAccess.create(MidiTransportProtocol.UMP)
+    }
+    application {
+        Window(
+            onCloseRequest = ::exitApplication,
+            state = rememberWindowState(),
+            title = "ComposeAudioControls Demo"
+        ) {
+            MainContent()
+        }
     }
 }
+
 
 @Preview
 @Composable
